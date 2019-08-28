07b51c041cb11f4fa2a5e884e267f75dbafb4c98
Pete Otaqui <49989933+pete-otaqui-sky@users.noreply.github.com>
2019-08-28 08:28:50 +0100

    Exported liftDice

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
 import { rollDice } from "./die";
 import { solve, solveTrie } from "./solve";
+import { liftDice } from "./board";
 
 // tslint:disable-next-line:no-var-requires
 const sowpodsTrie = require(`${__dirname}/word-lists/sowpods.trie.json`);
 // tslint:disable-next-line:no-var-requires
 const oedTrie = require(`${__dirname}/word-lists/oed.trie.json`);
 
-export { rollDice, solve, solveTrie, sowpodsTrie, oedTrie };
+export { rollDice, solve, solveTrie, sowpodsTrie, oedTrie, liftDice };
